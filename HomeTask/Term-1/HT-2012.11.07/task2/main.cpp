02a99f9e5ea1226d08a71fab951141f73966960e
Igor Ozernykh <Igor.Ozernykh@gmail.com>
2013-09-16 00:29:11 +0400

    file has been lost

---
+++ HomeTask/Term-1/HT-2012.11.07/task2/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "graph.h"
+#include <fstream>
+#define nullptr NULL
+
+int main()
+{
+	char filename[200];
+	std::cout << "Enter file name: ";
+	std::cin >> filename;
+	std::ifstream file(filename);
+	if (file != nullptr)
+	{
+		int amountOfCities;
+		file >> amountOfCities;
+		int amountOfRoads;
+		file >> amountOfRoads;
+		Graph *graph = createGraph(amountOfCities);
+		for (int i = 0; i < amountOfRoads; i++)
+		{
+			int city1;
+			int city2;
+			int length;
+			file >> city1;
+			file >> city2;
+			file >> length;
+			add(graph, city1, city2, length);
+		}
+		capture(graph);
+		printStatistics(graph, amountOfCities);
+		destroyGraph(graph, amountOfCities);
+	}
+	else 
+		std::cout << "File is not open\n";
+	char exit;
+	std::cout << "\nEnter any character to exit: ";
+	std::cin >> exit;
+}
